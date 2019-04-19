7a579f75ff02e552c61560aa9825c63208997791
Phillip Booker <phillippbooker@gmail.com>
2019-04-18 22:38:58 -0700

    Adds firebase. Stores player IDs.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,6 +11,14 @@ var draws = 0;
 var gameState;
 var players = 0;
 
+var userId = 17;
+var connectionId;
+var uniqueId;
+var playerId1;
+var playerId2;
+
+var latestChat = "";
+
 function resetGame(){
     pName1 = "";
     pChoice1 = "";
@@ -67,6 +75,95 @@ function setName(name){
     
 }
 
+function pushChat(message){
+    var newP = $("<p>");
+    newP.text(userId + ": " + message);
+    $("#chat").append(newP);
+
+    $("#chat-input").val("");
+
+    console.log($("#chat")[0].scrollTop);
+    $("#chat")[0].scrollTop = $("#chat")[0].scrollHeight;
+}
+
+// Initialize firebase
+var config = {
+    apiKey: "AIzaSyDerblX8eACRH313yiepS-e9ovK8qG5owk",
+    authDomain: "intro-project-a72be.firebaseapp.com",
+    databaseURL: "https://intro-project-a72be.firebaseio.com",
+    projectId: "intro-project-a72be",
+    storageBucket: "intro-project-a72be.appspot.com",
+    messagingSenderId: "305225461072"
+};
+
+firebase.initializeApp(config);
+
+// Create a variable to reference the database.
+var database = firebase.database();
+
+// Store node references here
+var connectionsRef = database.ref("/connections");
+var chatRef = database.ref("/chat");
+var gameRef = database.ref("/gameValues");
+
+
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+var connectedRef = database.ref(".info/connected");
+console.log("Connected: " + connectedRef);
+
+// When the client's connection state changes...
+connectedRef.on("value", function(snap) {
+    // If they are connected..
+    if (snap.val()) {
+
+        // Add user to the connections list.
+        connectionId = connectionsRef.push(true);
+
+        // uniqueId = connectionId.substring(1, 4);
+
+        // Remove user from the connection list when they disconnect.
+        connectionId.onDisconnect().remove();
+
+        for (key in connectionId){
+            console.log(key);
+        }
+        console.log("Connection id: " + connectionId);
+        console.log("Type: " + connectionId.key);
+    }
+}, function(errorObject){
+    console.log(errorObject.code);
+});
+
+connectionsRef.on("value", function(snap) {
+    console.log(snap.numChildren());
+    console.log(snap);
+});
+
+gameRef.on("value", function(snapshot){
+    players = snapshot.val().players;
+    playerId1 = snapshot.val().p1id;
+    playerId2 = snapshot.val().p2id;
+}, function(errorObject){
+    console.log(errorObject.code);
+});
+
+
+
+
+
+
+chatRef.on("value", function(snapshot){
+    // userId = snapshot.val().userId;
+
+    //Keep track of players in the game
+    latestChat = snapshot.val().chat;
+    pushChat(latestChat);
+    console.log(snapshot.numChildren());
+}), function(errorObject){
+    console.log(errorObject.code);
+};
+
+
 $("#submit-player").on("click", function(event){
     event.preventDefault();
     var input = $("#player-input").val().trim();
@@ -77,14 +174,27 @@ $("#submit-player").on("click", function(event){
 
         if(players == 1){
             $("#results").text("Enter name for Player 2");
+            gameRef.set({
+                p1id: connectionId.key,
+                p2id: playerId2,
+                players: players
+            });
+
+            console.log("Set the player info");
         } else if(players == 2){
             gameState = "p1";
             $("#results").text(pName1 + " is choosing...");
 
             $("#player-input").css("display", "none");
             $("#submit-player").css("display", "none");
+            gameRef.set({
+                p1id: playerId1,
+                p2id: connectionId.key,
+                players: players
+            });
         }
 
+        
         
     }
     $("#player-input").val("");
@@ -133,14 +243,13 @@ $("#submit-chat").on("click", function(event){
     event.preventDefault();
     var text = $("#chat-input").val().trim();
 
-    var newP = $("<p>");
-    newP.text(text);
-    $("#chat").append(newP);
+    chatRef.set({
+        chat: text,
+        userId: userId
+    });
+    connectionsRef.push(true);
 
-    $("#chat-input").val("");
-
-    console.log($("#chat")[0].scrollTop);
-    $("#chat")[0].scrollTop = $("#chat")[0].scrollHeight;
+    // pushChat(text);
 });
 
 $(document).ready(function(){
